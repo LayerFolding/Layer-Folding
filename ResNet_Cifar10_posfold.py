86ea99250269d649f5cc52b28d1e352b260abf50
LayerFolding <84841423+LayerFolding@users.noreply.github.com>
2021-05-31 10:39:47 +0300

    Update ResNet_Cifar10_posfold.py

--- ResNet_Cifar10_posfold.py
+++ ResNet_Cifar10_posfold.py
@@ -16,6 +16,7 @@ import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 import argparse
+import numpy as np
 
 parser = argparse.ArgumentParser(description='Propert ResNets for CIFAR10 in pytorch')
 
@@ -25,6 +26,8 @@ parser.add_argument('-lr', '--learning_rate', default=0.001, type=float,
                     metavar='LR', help='initial learning rate')
 parser.add_argument('-m', '--momentum', default=0.9, type=float, metavar='M',
                     help='momentum')
+parser.add_argument('-wd', '--weight_decay', '--wd', default=1e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)')
 parser.add_argument('-l','--lambda_reg', default=1/4, type=float,
                     metavar='L', help='lambda regularization (default: 1/4)')
 parser.add_argument('-d','--depth', default=20, type=int,
@@ -33,6 +36,7 @@ parser.add_argument('-d','--depth', default=20, type=int,
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+lambda_reg = 1 / 4
 
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
@@ -78,7 +82,6 @@ class alpha(nn.Module):
         return out
 
 
-
 # convolutions
 def conv(in_channels, out_channels, stride=1, kernel=1, bias=False):
     return nn.Conv2d(in_channels, out_channels, kernel_size=kernel, stride=stride, padding=kernel//2, bias=bias)
@@ -88,7 +91,10 @@ def conv(in_channels, out_channels, stride=1, kernel=1, bias=False):
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1, downsample=None):
         super(ResidualBlock, self).__init__()
-        self.conv1 = conv(in_channels, out_channels, stride, kernel=3)
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.stride = stride
+        self.conv1 = conv(in_channels, out_channels, stride, kernel=3, bias=False)
         self.bn1 = nn.BatchNorm2d(out_channels)
         self.activation = nn.ReLU()
         self.conv2 = conv(out_channels, out_channels, kernel=3)
@@ -96,19 +102,27 @@ class ResidualBlock(nn.Module):
         self.downsample = downsample
         self.alpha1 = alpha(0)
         self.alpha2 = alpha(0)
+        self.flag = False
+        if stride == 1:
+            self.new_conv = conv(in_channels, out_channels, stride, kernel=5, bias=True)
+        else:
+            self.new_conv = conv(in_channels, out_channels, stride, kernel=7, bias=True)
 
     def forward(self, x):
         residual = x
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.alpha1(out) + self.activation(out) - self.alpha1(self.activation(out))
-        out = self.conv2(out)
+        if self.alpha1.alpha.item() < 0.9 or self.flag:
+            out = self.conv1(x)
+            out = self.bn1(out)
+            out = self.alpha1(out) + self.activation(out) - self.alpha1(self.activation(out))
+            out = self.conv2(out)
+        else:
+            out = self.new_conv(x)
         out = self.bn2(out)
         if self.downsample:
             residual = self.downsample(x)
         out += residual
-        out = self.alpha2(out) + self.activation(out) - self.alpha2(self.activation(out))
-
+        if self.alpha2.alpha.item() < 0.9 or self.flag:
+            out = self.alpha2(out) + self.activation(out) - self.alpha2(self.activation(out))
         return out
 
 
@@ -117,7 +131,7 @@ class ResNet(nn.Module):
     def __init__(self, block, layers, num_classes=10):
         super(ResNet, self).__init__()
         self.in_channels = 16
-        self.conv1 = conv(3, 16, kernel=3)
+        self.conv1 = self.new_conv = conv(3, 16, kernel=3)
         self.bn1 = nn.BatchNorm2d(16)
         self.relu = nn.ReLU(inplace=True)
         self.layer1 = self.make_layer(block, 16, layers[0])
@@ -130,7 +144,7 @@ class ResNet(nn.Module):
         downsample = None
         if (stride != 1) or (self.in_channels != out_channels):
             downsample = nn.Sequential(
-                conv(self.in_channels, out_channels, stride=stride, kernel=1),
+                conv(self.in_channels, out_channels, stride=stride, kernel=1, bias=False),
                 nn.BatchNorm2d(out_channels))
         layers = []
         layers.append(block(self.in_channels, out_channels, stride, downsample))
@@ -153,25 +167,66 @@ class ResNet(nn.Module):
 
 
 if __name__ == "__main__":
-    
+
     # set params
     args = parser.parse_args()
     curr_lr = args.learning_rate
     momentum = args.momentum
     lambda_reg = args.lambda_reg
-    num_epochs = 100
+    num_epochs = args.epochs
     nb = int((args.depth - 2) / 6)
-    
+
     # create the model
-    file_name = f'models/resnet{nb * 6 + 2}.pt'
+    file_name = f'models/prefold_resnet{args.depth}.ckpt'
     model = ResNet(ResidualBlock, [nb, nb, nb]).to(device)
     state_dict = torch.load(file_name)
     alpha_arr = [module for module in model.modules() if isinstance(module, alpha)]
     model.load_state_dict(state_dict, strict=False)
-    model = model.to(device)
-    criterion = nn.CrossEntropyLoss()
 
-    # set params
+    for a in alpha_arr:
+        a.alpha.requires_grad = False
+
+    model = model.to(device)
+    model.eval()
+
+    # calculate the new kernels
+    index = 0
+    dim = 13
+    for ml in [model.layer1, model.layer2, model.layer3]:
+        for j in range(nb):
+            in_c = ml[j].in_channels
+            out_c = ml[j].out_channels
+            # calculate kernel
+            if alpha_arr[index].alpha.item() >= 0.9:
+                zeros = torch.zeros([1, in_c, dim, dim], requires_grad=True)
+                zeros = zeros.to(device)
+                zeros.retain_grad()
+                out = ml[j].conv1(zeros)
+                out = ml[j].bn1(out)
+                out = ml[j].conv2(out)
+                c_i, c_j = dim // 2, dim // 2
+                if ml[j].stride == 1:
+                    biases = out[0, :, dim // 2, dim // 2]
+                    Kernel = np.zeros([out_c, in_c, 5, 5])
+                    for i in range(out_c):
+                        biases[i].backward(retain_graph=True)
+                        Kernel[i] = zeros.grad[0, :, c_i - 2:c_i + 3, c_j - 2:c_j + 3].cpu().numpy()
+                        zeros.grad.zero_()
+                else:
+                    biases = out[0, :, dim // 4, dim // 4]
+                    Kernel = np.zeros([out_c, in_c, 7, 7])
+                    for i in range(out_c):
+                        biases[i].backward(retain_graph=True)
+                        Kernel[i] = zeros.grad[0, :, c_i - 3:c_i + 4, c_j - 3:c_j + 4].cpu().numpy()
+                        zeros.grad.zero_()
+                biases = biases.detach().cpu().numpy()
+                ml[j].new_conv.weight = nn.Parameter(torch.tensor(Kernel).float())
+                ml[j].new_conv.bias = nn.Parameter(torch.tensor(biases).float())
+            index += 2
+    model = model.to(device)
+    model.eval()
+
+    criterion = nn.CrossEntropyLoss()
 
     optimizer = torch.optim.SGD(model.parameters(), lr=curr_lr,
                                 momentum=momentum)
@@ -202,11 +257,6 @@ if __name__ == "__main__":
             loss.backward()
             optimizer.step()
 
-            # Constraint: alpha non negative
-            with torch.no_grad():
-                for a in alpha_arr:
-                    a.alpha[0].clip_(min=0.)
-
             if (i + 1) % 100 == 0:
                 print("Epoch [{}/{}], Step [{}/{}] Loss: {:.4f}\n"
                       .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
@@ -231,9 +281,9 @@ if __name__ == "__main__":
 
             print('Accuracy of the model on the test images: {} %'.format(100 * correct / total))
     # Save the model checkpoint
-    torch.save(model.state_dict(), f'models/prefold_resnet{2 + 6 * nb}.ckpt')
+    torch.save(model.state_dict(), f'models/postfold_resnet{args.depth}.ckpt')
     # acc.append(correct / total)
-    text_file = open(f"models/acc_resnet{2 + nb * 6}.txt", "w")
+    text_file = open(f"models/postfold_resnet{args.depth}.txt", "w")
     text_file.write(str(correct / total))
     text_file.write("\n")
     alpha_arr_val = []
